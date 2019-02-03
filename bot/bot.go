3d3fe71768c848ffdb4dde7aed3742efc4e8ba7b
Benjamin Moskau <mosk0021@algonquinlive.com>
2019-02-02 23:25:26 -0500

    This adds the config and bot folders

---
+++ bot/bot.go
@@ -0,0 +1,45 @@
+package bot
+
+import (
+	"BlondeBot/utility"
+	"fmt"
+
+	"BlondeBot/config"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+var BotID string
+var goBot *discordgo.Session
+
+func Start() {
+
+	goBot, err := discordgo.New("Bot " + config.Token)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	u, err := goBot.User("@me")
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	BotID = u.ID
+
+	goBot.AddHandler(utility.MessageHandler)
+
+	err = goBot.Open()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Println("Bot is running!")
+
+	<-make(chan struct{})
+	return
+}
